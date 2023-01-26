a1ca61d2937b70626c62104d7ac16be0478e487c
CinemaCafe <65265249+CinemaCafe@users.noreply.github.com>
2023-01-26 16:09:39 +0800

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,26 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// add dbcontex and connection string.
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-// add Cors
-builder.Services.AddCors();
-
+// build a static services collection 
+builder.Services.AddApplicationServices(builder.Configuration);
+// JWT
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 // cors option
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
+
+// to ask do you have a valid token?
+app.UseAuthentication();
+// okay you have a valid token.
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Use(async (context, next) =>
